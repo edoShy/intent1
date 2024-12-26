3bf9e4a27c7058e3607f963a2db88bd7915f8d0f
edoshalemhymowitz <edoshy@gmail.com>
2024-12-26 11:00:50 +0200

    huktfiu

--- app/src/main/java/com/example/intent/MainActivity.java
+++ app/src/main/java/com/example/intent/MainActivity.java
@@ -1,24 +1,44 @@
 package com.example.intent;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button openBrowserButton = findViewById(R.id.openBrowserButton);
+        Button makeCallButton = findViewById(R.id.makeCallButton);
+        Button shareTextButton = findViewById(R.id.shareTextButton);
+
+        openBrowserButton.setOnClickListener(v -> {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com"));
+            startActivity(browserIntent);
+        });
+
+        makeCallButton.setOnClickListener(v -> {
+            Intent callIntent = new Intent(Intent.ACTION_DIAL);
+            callIntent.setData(Uri.parse("tel:1234567890"));
+            startActivity(callIntent);
+        });
+        Log.e("SSSSS","before");
+
+        shareTextButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ShareActivity.class);
+
+            Log.e("SSSSS","after");
+
+            startActivity(intent);
+
+            Log.e("SSSSS","after start activity");
+
         });
     }
 }
